0015789ac20a45f2fb8ba560d90805b36d359ec9
happiguru <hguruman@gmail.com>
2021-06-08 22:34:41 +0400

    Add style for the home page

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,24 +2,25 @@
 
 @section('content')
 
-<div class="hero"></div>
+<div class="hero">
     <div class="hero-content">
         <h1>Daily Dose of Cute Animals!</h1>
         <h2>Only $10/mo</h2>
         <div class="cta">
             <p>We promise you won't regret it.</p>
-            <a href="/subscribe">Subscribe</a>
+            <a href="/subscribe" class="btn btn-danger btn-lg">Subscribe</a>
         </div>
     </div>
-
-    <section>
-        <div class="container">
-            <div class="section-header">
-                <h2>Latest Animals</h2>
-            </div>
-            {{-- Post will go here --}}
-        </div>
-    </section>
 </div>
+    
+<section>
+    <div class="container">
+        <div class="section-header">
+            <h2>Latest Animals</h2>
+        </div>
+        {{-- Post will go here --}}
+    </div>
+</section>
+
 
 @endsection
