70eae74e1c47c48b791dfe454b9c9cdc55b6e314
happiguru <hguruman@gmail.com>
2021-06-09 11:32:22 +0400

    Add user authentication using bootstrap-laravel-ui

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,36 +1,45 @@
-<!DOCTYPE html>
-<html lang="en">
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    {{-- stylesheet --}}
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">
-     <!-- Bootstrap CSS -->
+
+    <!-- Styles -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
     <link href="css/app.css" rel="stylesheet" />
-    <title>Cashier - App</title>
 </head>
 <body>
-    {{-- Header --}}
-    <div id="site-header">
-        @include('partials.header')
+    <div id="app">
+        {{-- Header --}}
+        <div id="site-header">
+            @include('partials.header')
+        </div>
+        {{-- Main --}}
+        <main id="site-main py-4">
+            @yield('content')
+        </main>
+
+        {{-- footer --}}
+
+        <div id="site-footer">
+            @include('partials.footer')
+        </div>
+        {{-- javascript --}}
+        <!-- Option 1: Bootstrap Bundle with Popper -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+        <script src="js/app.js"></script>
     </div>
-
-    {{-- Main --}}
-    <main id="site-main">
-        @yield('content')
-    </main>
-
-    {{-- stylesheet --}}
-
-    <div id="site-footer">
-        @include('partials.footer')
-    </div>
-    {{-- javascript --}}
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
-    <script src="js/app.js"></script>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,16 @@ use Illuminate\Support\Facades\Route;
 // Site routes
 
 Route::get('/', 'App\Http\Controllers\SiteController@showHome');
-Route::get('{slug}', 'App\Http\Controllers\SiteController@showPost');
+
 
 // Authentication routes
+Auth::routes();
+// Route::get('login', 'App\Http\Controllers\Auth\LoginController@showLoginForm');
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 // Subscription routes
 
 
 // Account routes
+Route::get('{slug}', 'App\Http\Controllers\SiteController@showPost');
