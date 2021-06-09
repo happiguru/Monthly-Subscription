af6cca46fe1bbaaee6004e8818ccc3e3b8b1c235
happiguru <hguruman@gmail.com>
2021-06-09 13:24:24 +0400

    All subscribtion button plus its css styles

--- resources/views/pages/subscribe.blade.php
+++ resources/views/pages/subscribe.blade.php
@@ -14,24 +14,54 @@
         @if(Auth::guest())
             <div class="section-header">
                 <h2>User Info</h2>
-                <div class="form-group">
-                    <label>Name</label>
-                    <input type="text" class="form-control" name="name">
-                </div>
-                <div class="form-group">
-                    <label>Email</label>
-                    <input type="email" class="form-control" name="email">
-                </div>
-                <div class="form-group">
-                    <label>Password</label>
-                    <input type="password" class="form-control" name="password">
-                </div>
             </div>
+            <div class="form-group">
+                <label>Name</label>
+                <input type="text" class="form-control" name="name">
+            </div>
+            <div class="form-group">
+                <label>Email</label>
+                <input type="email" class="form-control" name="email">
+            </div>
+            <div class="form-group">
+                <label>Password</label>
+                <input type="password" class="form-control" name="password">
+            </div>
+            
         @endif
 
         {{-- subscription info --}}
             <div class="section-header">
                 <h2>Subscription Info</h2>
+            </div>
+            <div class="row">
+                <div class="col-4">
+                    <div class="subscription-option">
+                        <input type="radio" id="plan-bronze" name="plan" value="bronze" checked>
+                        <label for="plan-bronze">
+                            <span class="plan-price">$5 <small>/mon</small></span>
+                            <span class="plan-name">Bronze</span>
+                        </label>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="subscription-option">
+                        <input type="radio" id="plan-silver" name="plan" value="silver">
+                        <label for="plan-silver">
+                            <span class="plan-price">$15 <small>/mon</small></span>
+                            <span class="plan-name">Bronze</span>
+                        </label>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="subscription-option">
+                        <input type="radio" id="plan-gold" name="plan" value="gold">
+                        <label for="plan-gold">
+                            <span class="plan-price">$20 <small>/mon</small></span>
+                            <span class="plan-name">Bronze</span>
+                        </label>
+                    </div>
+                </div>
             </div>
         {{-- credit card info --}}
 
